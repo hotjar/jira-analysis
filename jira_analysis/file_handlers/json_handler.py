5368c413f2fca8fd87cba592c6534c88f91acc06
Scott Walton <scott.walton@hotjar.com>
2020-06-03 14:44:54 +0100

    Added type hints to JSON.

--- jira_analysis/file_handlers/json_handler.py
+++ jira_analysis/file_handlers/json_handler.py
@@ -1,13 +1,15 @@
 import json
 
 from datetime import date, datetime
+from typing import Any, IO, Optional
 
 
 class _Encoder(json.JSONEncoder):
-    def default(self, obj):
+    def default(self, obj: Any) -> Optional[Any]:
         if isinstance(obj, (date, datetime)):
             return obj.isoformat()
+        return None
 
 
-def dump(obj, to):
+def dump(obj: Any, to: IO[str]) -> None:
     return json.dump(obj, to, cls=_Encoder, indent=2)
